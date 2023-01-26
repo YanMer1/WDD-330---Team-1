fe2f6a74ad610f13634a78acd49cfc37a131ccdb
DiabGarren <garrendiab@gmail.com>
2023-01-26 12:01:19 +0200

    Commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import {
+  updateCartIcon,
+  getLocalStorage,
+  setLocalStorage
+} from './utils.mjs';
+
+if (!getLocalStorage('so-cart').length) {
+  setLocalStorage('so-cart', []);
+}
+
+updateCartIcon();
