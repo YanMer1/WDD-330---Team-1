fdd2174d43fe655b72bd5515767bdfa837df4b9d
DiabGarren <garrendiab@gmail.com>
2023-02-02 13:18:58 +0200

    Cart item subtotal + quantity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,12 @@
 import { getLocalStorage, setLocalStorage, updateCartIcon } from './utils.mjs';
 
+let inCartItems = [];
+let inCartQty = [];
+let displayed = [];
+
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
+  inCart(cartItems);
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   if (htmlItems.length == 0) {
     document.querySelector('.product-list').innerHTML = '<h1>Your cart is empty :(</h1>';
@@ -10,7 +15,22 @@ function renderCartContents() {
   }
 }
 
+function inCart(cartItems) {
+  for (let item of cartItems) {
+    if (!inCartItems.includes(item.Id)) {
+      inCartItems.push(item.Id);
+      inCartQty.push(1);
+      displayed.push(false);
+    } else {
+      inCartQty[inCartItems.indexOf(item.Id)] += 1;
+    }
+  }
+}
+
 function cartItemTemplate(item) {
+  if (displayed[inCartItems.indexOf(item.Id)]) {
+    return '';
+  } else {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
@@ -22,13 +42,16 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__quantity">qty: ${inCartQty[inCartItems.indexOf(item.Id)]}</p>
   <p class="cart-card__suggested-price discount suggested-price">$${item.SuggestedRetailPrice}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <p class="">$${item.FinalPrice * inCartQty[inCartItems.indexOf(item.Id)]}</p>
 </li>
 <button class="remove-item" id=${item.Id}>X</button>`;
-
+  
+    displayed[inCartItems.indexOf(item.Id)] = true;
   return newItem;
+  }
 }
 
 function removeCartItem(item) {
@@ -41,7 +64,9 @@ function removeCartItem(item) {
         cartItems.splice(i, 1);
 
         setLocalStorage('so-cart', cartItems);
-
+        inCartItems = [];
+        inCartQty = [];
+        displayed = [];
         renderCartContents();
         updateCartIcon();
         return;
