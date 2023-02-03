7fe9a571f97e64918d4aacf8f1f231897b2d36bc
YanMer1 <mer21002@byui.edu>
2023-02-03 18:56:15 +0000

    Commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,11 @@
-import { updateCartIcon, getLocalStorage, setLocalStorage, loadheaderFooter } from './utils.mjs';
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import { loadHeaderFooter } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
-/* 
-Convert cart item in local storage from JSON to Array.
-  If the length can't be return then it's not an array.
-  Set the local storage to an empty array.
-  This is so that we can add and remove items from the cart.
-*/
-if (!getLocalStorage('so-cart').length) {
-  setLocalStorage('so-cart', []);
-}
+loadHeaderFooter();
 
-loadheaderFooter();
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const listing = new ProductList("Tents", dataSource, element);
 
-const dataSource = new ProductData('tents');
-
-const productList = new ProductList('tents', dataSource, document.querySelector('.product-list'));
-
-function init() {
-  productList.init();
-  
-  updateCartIcon();
-}
-
-setTimeout(init, 25);
+listing.init();
