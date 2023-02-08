7ae8777dfbc190d6d93d8a70b9c91808c41f4aca
DiabGarren <garrendiab@gmail.com>
2023-02-08 23:23:19 +0200

    Add module

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,83 +1,24 @@
-import { getLocalStorage, setLocalStorage, updateCartIcon, loadheaderFooter } from './utils.mjs';
+import { updateCartIcon, loadheaderFooter } from './utils.mjs';
+import ShoppingCart from './ShoppingCart.mjs';
+import { removeCartItem } from './ShoppingCart.mjs';
 
-let inCartItems = [];
-let cartItemsDisplayed = [];
-
-function renderCartContents() {
-  const cartItems = getLocalStorage('so-cart');
-  inCart(cartItems);
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  if (htmlItems.length == 0) {
-    document.querySelector('.product-list').innerHTML = '<h1>Your cart is empty :(</h1>';
-  } else {
-    document.querySelector('.product-list').innerHTML = htmlItems.join('');
-  }
-}
-
-function inCart(cartItems) {
-  for (let item of cartItems) {
-    if (!inCartItems.includes(item.Id)) {
-      inCartItems.push(item.Id);
-      cartItemsDisplayed.push([1, false]);
-    } else {
-      cartItemsDisplayed[inCartItems.indexOf(item.Id)][0] += 1;
-    }
-  }
-}
-
-function cartItemTemplate(item) {
-  if (cartItemsDisplayed[inCartItems.indexOf(item.Id)][1]) {
-    return '';
-  } else {
-    const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: ${cartItemsDisplayed[inCartItems.indexOf(item.Id)][0]}</p>
-  <p class="cart-card__price">Each: $${item.FinalPrice}</p>
-  <p class="cart-card__subtotal">Subtotal: $${item.FinalPrice * cartItemsDisplayed[inCartItems.indexOf(item.Id)][0]}</p>
-</li>
-<button class="remove-item" id=${item.Id}>X</button>`;
-
-    cartItemsDisplayed[inCartItems.indexOf(item.Id)][1] = true;
-    return newItem;
-  }
-}
-
-function removeCartItem(item) {
-  const cartItems = getLocalStorage('so-cart');
-
-  if (item.target.classList == 'remove-item') {
-    for (let i = 0; i < cartItems.length; i++) {
-      if (item.target.id == cartItems[i].Id) {
-
-        cartItems.splice(i, 1);
-
-        setLocalStorage('so-cart', cartItems);
-        inCartItems = [];
-        cartItemsDisplayed = [];
-        renderCartContents();
-        updateCartIcon();
-        return;
-      }
-    }
-  }
-}
 
 loadheaderFooter();
 
+const cart = new ShoppingCart('so-cart', '.product-list');
+
+function removeItemHandler(item) {
+  let removed = removeCartItem(item);
+  if (removed) {
+    cart.renderCartContents();
+    updateCartIcon();
+  }
+}
 
 function init() {
-  renderCartContents();
+  cart.renderCartContents();
   updateCartIcon();
-  document.body.addEventListener('click', removeCartItem);
+  document.body.addEventListener('click', removeItemHandler);
 }
 
 setTimeout(init, 25);
