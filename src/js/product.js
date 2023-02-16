f5dce2d2eeb6aa91d645b30b6ce839e6910eaa8c
DiabGarren <garrendiab@gmail.com>
2023-02-16 12:46:16 +0200

    ProductData renamed ExternalServices

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,12 @@
 import { getParam, loadheaderFooter } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import ExternalServices from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
 
 loadheaderFooter();
 const category = getParam('category');
 
 const productId = getParam('product');
-const dataSource = new ProductData(category);
+const dataSource = new ExternalServices(category);
 
 const productDetails = new ProductDetails(productId, dataSource);
 productDetails.init();
--- src/js/productListing.js
+++ src/js/productListing.js
@@ -1,12 +1,12 @@
 import { updateCartIcon, loadheaderFooter, getParam } from './utils.mjs';
-import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
+import ExternalServices from './ProductData.mjs';
 
 
 loadheaderFooter();
 const category = getParam('category');
 
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 
 const listElement = document.querySelector('.product-list');
 
